3a74e2e4419386ecc6d666d48bef5974415d2d5a
Nikola Gocic <ngocic97@gmail.com>
2020-03-15 14:26:37 +0100

    Povezana baza

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const db = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+db();
 
 app.get("/", (req, res) => res.send("API Running"));
 
