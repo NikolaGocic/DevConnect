85efd58545613e48ab3cd2469b0af68c0756d836
Nikola Gocic <ngocic97@gmail.com>
2020-03-15 18:35:18 +0100

    Model and routes added

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const db = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 db();
 
-app.get("/", (req, res) => res.send("API Running"));
+pp.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profiles", require("./routes/api/profiles"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
